28fb3f4445f1407a1858da335768470bc3d6fb4e
ALPHAKINGG <75511385+Alphakingg-ai@users.noreply.github.com>
2022-02-11 07:09:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const { spawn } = require("child_process");
 const path = require("path");
 const fs = require("fs");
 var isRunning = false;
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 function start(file) {
   if (isRunning) return;
